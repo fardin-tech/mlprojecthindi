6e1ae3799a14b23b3ca3565fdfbf1e1fb6dae072
Fardin <fardeenrangrezz4@gmail.com>
2024-03-27 20:51:08 +0530

    data transfomation

--- src/mlproject/utlis.py
+++ src/mlproject/utlis.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 import pymysql
 import mysql.connector
 
+import pickle
+import numpy as np
+
 load_dotenv()
 host=os.getenv("host")
 user=os.getenv("user")
@@ -31,3 +34,11 @@ def read_sql_data():
         return df
     except Exception as ex:
         raise CustomException(ex,sys)
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
